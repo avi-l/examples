6ac474cd761d5d54e0f6d62e2b56a409f067bf3f
avi-leeker <68050609+avi-leeker@users.noreply.github.com>
2021-07-21 19:29:16 +0300

    Update signIn.jsx

--- react/user_related_components/user/signIn.jsx
+++ react/user_related_components/user/signIn.jsx
@@ -10,17 +10,19 @@ import PassResetForm from "./userLogin/PassResetForm";
 import ErrorsModal from './shared/ErrorsModal';
 import storeContext from "../../stores/storeContext";
 import { observer } from "mobx-react";
+import { Card, Nav } from "react-bootstrap";
+import './Login.css'
 
 const SignIn = observer(() => {
   const store = useContext(storeContext);
   const { loginStore } = store;
-  const {
-    isSignInForm,
-    isSignUpForm,
-    isSendPassResetCodeForm,
-    isForgotPassForm,
-    isVerifyCodeForm,
-    isPassResetForm
+  const { isSignInForm, setIsSignInForm,
+    isSignUpForm, setIsSignUpForm,
+    isVerifyCodeForm, setIsVerifyCodeForm,
+    isSendPassResetCodeForm, setIsSendPassResetCodeForm,
+    isForgotPassForm, setIsForgotPassForm,
+    isPassResetForm, setIsPassResetForm,
+    setEmail, setUsername, setPassword, setPasswordCopy,
   } = loginStore;
 
   useEffect(() => {
@@ -31,20 +33,60 @@ const SignIn = observer(() => {
       .catch((err) => console.error(err));
   }, []);
 
+  const clearForms = () => {
+    setIsSignInForm(false);
+    setIsSignUpForm(false);
+    setIsPassResetForm(false);
+    setIsForgotPassForm(false);
+    setIsVerifyCodeForm(false);
+    setIsSendPassResetCodeForm(false);
+    setEmail('');
+    setPassword('');
+    setPasswordCopy('');
+    setUsername('');
+  }
+
   return (
-    <div id='logreg-login'>
-      <div className='row no-gutters'>
-        <div className='col-md-4'></div>
-        {/*    <div id="logreg-forms" className="col-md-17">*/}
-        {isSignInForm && <SignInForm />}
-        {isSignUpForm && <SignUpForm />}
-        {isSendPassResetCodeForm && <SendPassResetCodeForm />}
-        {isForgotPassForm && <ForgotPasswordForm />}
-        {isVerifyCodeForm && <VerifyCodeForm />}
-        {isPassResetForm && <PassResetForm />}
-        <ErrorsModal />
-        {/*    </div>*/}
-      </div>
+    <div className='login-body'>
+      <Card className="mx-auto login-card">
+        <Card.Header className="mx-auto login-card-header">
+          <Card.Title className=" login-card-title">
+            <i className="far fa-lightbulb" /> Login
+          </Card.Title>
+          <Nav justify variant="tabs" className="login-nav-tabs" defaultActiveKey="#signin">
+            <Nav.Item>
+              <Nav.Link className="login-nav-tabs" eventKey="#signin" title="Sign In!"
+                onClick={() => {
+                  clearForms();
+                  setIsSignInForm(true);
+                }}><i className="fas fa-sign-in-alt"></i></Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link className="login-nav-tabs" eventKey="#signup" title="Sign Up!"
+                onClick={() => {
+                  clearForms();
+                  setIsSignUpForm(true);
+                }}><i className="fas fa-user-plus"></i></Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link className="login-nav-tabs" eventKey="#forgotpass" title="Forgot Password?"
+                onClick={() => {
+                  clearForms()
+                  setIsSendPassResetCodeForm(true);
+                }}><i className="fas fa-key"></i>  ?</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Header>
+        <Card.Body className="login-card-body">
+          {isSignInForm && <SignInForm />}
+          {isSignUpForm && <SignUpForm />}
+          {isSendPassResetCodeForm && <SendPassResetCodeForm />}
+          {isForgotPassForm && <ForgotPasswordForm />}
+          {isVerifyCodeForm && <VerifyCodeForm />}
+          {isPassResetForm && <PassResetForm />}
+          <ErrorsModal />
+        </Card.Body>
+      </Card>
     </div>
   );
 });
