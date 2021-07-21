e66cf334baf2cffe94cb9aaf01ee3ca656e3eeea
avi-leeker <68050609+avi-leeker@users.noreply.github.com>
2021-07-21 12:27:09 +0300

    Update userManagement.js

--- react/user_related_components/user/userManagement.js
+++ react/user_related_components/user/userManagement.js
@@ -1,6 +1,8 @@
 import { awsconfig } from './cognitoCredentials';
 import { Auth } from 'aws-amplify';
 import { forceReload } from '../../utilities/forceReload';
+import { getUser, getProfileUser } from './user_api';
+import { CognitoUser } from 'amazon-cognito-identity-js';
 
 Auth.configure(awsconfig);
 
@@ -69,9 +71,115 @@ export const isUserLoggedIn = async (isBypassCache) => {
     return Auth.currentAuthenticatedUser({ bypassCache: isBypassCache })
 };
 
-export const updateCognitoUserAttributes = async (user, contributorCode) => {
+export const cognitoEmailUsed = async (email) => {
+    return signIn(email, '123')
+        .then(res => {
+            return false;
+        })
+        .catch(error => {
+            const code = error.code;
+            console.error(error);
+            switch (code) {
+                case 'UserNotFoundException':
+                    return false;
+                case 'NotAuthorizedException':
+                    return true;
+                case 'PasswordResetRequiredException':
+                    return true;
+                case 'UserNotConfirmedException':
+                    return true;
+                default:
+                    return false;
+            }
+        });
+}
+export const setUserInStore = async (user, setUser, isBypassCache) => {
+    await isUserLoggedIn(isBypassCache)
+        .then(U => {
+            const { identities } = U.attributes;
+            !identities
+                ? setUser({
+                    ...user,
+                    userId: U.attributes.sub,
+                    userHandle: U.attributes.preferred_username || U.username,
+                    isContributor: !!U.attributes['custom:contributorCode'],
+                    avatar: U.attributes.picture || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+                    authProvider: 'Cognito'
+                })
+                :
+                getUser({ userId: U.attributes.sub })
+                    .then(localDbUser => {
+                        if (!localDbUser.data) {
+                            return false;
+                        }
+                        else {
+                            setUser({
+                                ...user,
+                                userId: U.attributes.sub,
+                                userHandle: localDbUser.data?.userHandle,
+                                firstName: localDbUser.data?.firstName,
+                                lastName: localDbUser.data?.lastName,
+                                isContributor: !!U.attributes['custom:contributorCode'],
+                                avatar: localDbUser.data?.avatar || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+                                authProvider: localDbUser.data?.authProvider,
+                            })
+                            return true;
+                        }
+                    })
+                    .catch((err) => {
+                        return { error: err };
+                    });
+        }
+        )
+        .catch((err) => console.error(err));
+}
+
+export const setUserProfileData = async (user, setUser, isFullDetails) => {
+    return getUser({ userId: user.userId, isFullDetails })
+        .then(async localDbUser => {
+            if (!localDbUser.data) {
+
+                return false;
+            }
+            else {
+                setUser({
+                    ...user,
+                    userHandle: localDbUser.data?.userHandle,
+                    authProvider: localDbUser.data?.authProvider,
+                    email: localDbUser.data?.email,
+                    firstName: localDbUser.data?.firstName,
+                    lastName: localDbUser.data?.lastName,
+                    mobilePhone: localDbUser.data?.mobilePhone,
+                    avatar: localDbUser.data?.avatar || "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+                    followers: localDbUser.data?.followers,
+                    following: localDbUser.data?.following,
+                    address: {
+                        address1: localDbUser.data?.address1,
+                        address2: localDbUser.data?.address2,
+                        city: localDbUser.data?.city,
+                        state: localDbUser.data?.state,
+                        zip: localDbUser.data?.zip,
+                        country: localDbUser.data?.country,
+                    }
+                })
+                return true;
+            }
+        })
+        .catch((err) => {
+            return { error: err };
+        });
+}
+
+export const updateCognitoUserAttributes = async (user, attributes) => {
     return await Auth.updateUserAttributes(user, {
-        'custom:contributorCode': contributorCode
+        ...attributes.contributorCode
+            ? { 'custom:contributorCode': attributes.contributorCode }
+            : {
+                // 'email': attributes.email,
+                'family_name': attributes.family_name,
+                'given_name': attributes.given_name,
+                'preferred_username': attributes.preferred_username
+            }
     })
         .then(res => {
             return res;
@@ -101,7 +209,6 @@ export const deleteUserInAws = async (isBypassCache) => {
                 }
                 Auth.signOut({ global: true })
                 return data;
-
             })
         }).catch(err => {
             return err;
@@ -125,10 +232,12 @@ export const changeUserPass = async (userObj, oldPassword, newPassword) => {
 export const logout = async () => {
     await Auth.signOut({ global: true })
         .then(res => {
+            localStorage.clear();
             forceReload('/');
         })
         .catch(err => {
             localStorage.clear();
+            forceReload('/');
             return err;
         });
 }
