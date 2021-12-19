69069b38f5c764121e36c5199f09916835c15649
Avi Leeker <avi.leeker@gmail.com>
2021-12-19 18:58:09 +0200

    server.js update

--- node/server.js
+++ node/server.js
@@ -5,9 +5,17 @@ const PORT = process.env.PORT || 4000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const getVimeoMetadata = require('./routes/getVimeoMetadata');
 const UsersRoute = require('./routes/userRoutes');
 const ConversationRoute = require('./routes/conversationRoutes');
 const MessageRoute = require('./routes/messageRoutes');
+
+const connectToLocalDb = () => {
+    mongoose.connect(config.localDB, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+        if (err) console.error(err)
+        console.log('Local db connected');
+    })
+};
 
 mongoose.connect(process.env.DB_CONNECT_STRING, {
     useUnifiedTopology: true,
@@ -16,9 +24,12 @@ mongoose.connect(process.env.DB_CONNECT_STRING, {
     .then(
         () => {
             console.log('Remote database is connected');
+            getVimeoMetadata.pollDb();
+            setInterval(() => getVimeoMetadata.pollDb(), 9000000);
         },
         err => {
             console.log('Cannot connect to the remote database' + err);
+            connectToLocalDb();
         }
     );
 
@@ -28,6 +39,7 @@ app.use(bodyParser.json());
 app.use('/users', UsersRoute);
 app.use('/message', MessageRoute);
 app.use('/conversation', ConversationRoute);
+
 
 app.get('/health', (req, res) => res.send('Hello healthy app!'))
 
@@ -45,7 +57,7 @@ const io = require('socket.io')(8900, {
 let users = []
 const addUser = (userId, socketId) => {
     !users.some(user => user.userId === userId) &&
-        users.push({ userId, socketId })
+    users.push({userId, socketId})
 }
 const removeUser = (socketId) => {
     users = users.filter(user => user.socketId !== socketId)
@@ -62,51 +74,51 @@ io.on('connection', (socket) => {
         socket.emit('connected')
     })
     //send message
-    socket.on('sendMessage', ({ senderId, receiverId, text, timestamp }) => {
+    socket.on('sendMessage', ({senderId, receiverId, text, timestamp}) => {
         const user = getUser(receiverId);
         io.to(user?.socketId).emit('getMessage', {
-          senderId,
-          receiverId,
-          text,
-          timestamp
-        });       
-      });
-     
+            senderId,
+            receiverId,
+            text,
+            timestamp
+        });
+    });
+
     //delete message
     socket.on('deleteMessage', ({receiverId, messageId}) => {
         const user = getUser(receiverId);
         io.to(user?.socketId).emit('deleteMessage', {
             messageId,
         });
-      });
+    });
     //listen for user typing..
-    socket.on('userStartedTyping', ({ senderId, receiverId }) => {
+    socket.on('userStartedTyping', ({senderId, receiverId}) => {
         const user = getUser(receiverId);
         io.to(user?.socketId).emit('userStartedTyping', {
-          senderId,
-          receiverId
+            senderId,
+            receiverId
         });
-      });
-    socket.on('userStoppedTyping', ({ senderId, receiverId }) => {
+    });
+    socket.on('userStoppedTyping', ({senderId, receiverId}) => {
         const user = getUser(receiverId);
         io.to(user?.socketId).emit('userStoppedTyping', {
-          senderId,
-          receiverId
+            senderId,
+            receiverId
         });
-      });
+    });
     //conversation selected
-    socket.on('selectedConversation', ({ senderId, receiverId }) => {
+    socket.on('markMsgsRead', ({senderId, receiverId}) => {
         const user = getUser(senderId);
         io.to(user?.socketId).emit('markMsgsRead', {
-          senderId,
-          receiverId
+            senderId,
+            receiverId
         });
-      });
+    });
     //when disconnect
     socket.on('disconnect', () => {
         socket.emit('disconnected');
         removeUser(socket.id)
         io.emit('getUsers', users)
-        
     })
-})
+
+});
