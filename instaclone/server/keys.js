ee241babb19bdbf59b2cf431f662a17cb21fc51a
Avi Leeker <avi.leeker@gmail.com>
2022-04-24 09:35:12 +0300

    update

--- instaclone/server/keys.js
+++ instaclone/server/keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    MONGOURI:'mongodb+srv://mongo_db_user01:JPEDAcjaiaxX38Cz@dev-mongodb-cluster.1hzwq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    JWT_SECRET:"ImWalkinOnSunshineWooOooh"
+    MONGOURI:'',
+    JWT_SECRET:""
 }
